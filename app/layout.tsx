5024a63849f042b18ee3647954f1c5d4fa4a9428
Brian Olpin <bolpin@gmail.com>
2024-11-07 15:13:04 -0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { spectral } from '@/app/ui/fonts';
+import { syncopate } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { Syncopate } from 'next/font/google';
+import { Spectral } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({weight: '400'});
+export const syncopate = Syncopate({weight: '400'});
+export const spectral = Spectral({weight: '400'});
+export const inter = Inter({weight: '300'});
