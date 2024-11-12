aa08566ea7ef7be060b41bb674994f297f097a8b
Brian Olpin <bolpin@gmail.com>
2024-11-12 15:37:30 -0800

    removing font stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { spectral } from '@/app/ui/fonts';
-import { syncopate } from '@/app/ui/fonts';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function RootLayout({
