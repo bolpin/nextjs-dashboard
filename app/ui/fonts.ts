e99a1c56171eb27e081b2830ad598e3f903f2e85
Brian Olpin <bolpin@gmail.com>
2024-11-12 15:20:21 -0800

    removing test fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,5 @@
 import { Inter } from 'next/font/google';
-import { Syncopate } from 'next/font/google';
-import { Spectral } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const lusitana = Lusitana({weight: '400'});
-export const syncopate = Syncopate({weight: '400'});
-export const spectral = Spectral({weight: '400'});
 export const inter = Inter({weight: '300'});
